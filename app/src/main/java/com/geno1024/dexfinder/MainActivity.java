3c11f353d4db3f810507a7c7b087e9a78162bdde
Y. Z. Chen <754097987@qq.com>
2019-02-19 17:46:56 +0800

    [Update] and final?

--- app/src/main/java/com/geno1024/dexfinder/MainActivity.java
+++ app/src/main/java/com/geno1024/dexfinder/MainActivity.java
@@ -2,6 +2,7 @@ package com.geno1024.dexfinder;
 
 import android.app.*;
 import android.os.*;
+import java.io.*;
 
 public class MainActivity extends Activity 
 {
@@ -9,6 +10,15 @@ public class MainActivity extends Activity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        try
+	  	    {
+	         		java.lang.Process p = Runtime.getRuntime().exec("su");
+			         p.getOutputStream().write("ps | grep dex2oat\n".getBytes());
+			         android.widget.TextView t = new android.widget.TextView(this);
+			         t.setText(new BufferedReader(new InputStreamReader(p.getInputStream())).readLine());
+            setContentView(t);
+	      	}
+		      catch (IOException e)
+		      {}
     }
 }
